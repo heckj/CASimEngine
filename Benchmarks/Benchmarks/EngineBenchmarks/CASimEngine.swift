1553ede167b84911477ff8a2928b6495d22009a5
Joe Heck <heckj@mac.com>
2024-11-09 15:38:10 -0800

    fleshing out whole thing with light copy

--- Benchmarks/Benchmarks/EngineBenchmarks/CASimEngine.swift
+++ Benchmarks/Benchmarks/EngineBenchmarks/CASimEngine.swift
@@ -8,6 +8,7 @@ import Voxels
 let benchmarks = {
     Benchmark("sim iteration", configuration: .init(maxDuration: .seconds(9))) { benchmark in
         let bounds = VoxelBounds(min: .init(0, 0, 0), max: .init(99, 99, 99))
+        // 1_000_000
         var seed = VoxelArray(bounds: bounds, initialValue: 0)
         for idx in bounds.y(0 ... 0).indices {
             seed[idx] = 1
